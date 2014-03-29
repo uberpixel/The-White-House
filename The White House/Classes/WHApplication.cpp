667d54601bcd47199f88a209a9579faa1786b211
Sidney Just <Sidney.just@gmail.com>
2014-03-29 15:32:41 +0100

    Added a shitty cross-hair

--- The White House/Classes/WHApplication.cpp
+++ The White House/Classes/WHApplication.cpp
@@ -22,8 +22,27 @@ namespace WH
 
 		RN::World *world = new World();
 		RN::WorldCoordinator::GetSharedInstance()->LoadWorld(world->Autorelease());
+		
+		_crosshair = new RN::UI::ImageView(RN::UI::Image::WithFile("Textures/shottyiron.png"));
+		_crosshair->SetScaleMode(RN::UI::ScaleMode::ProportionallyDown);
+		
+		_crosshairWidget = new RN::UI::Widget(RN::UI::Widget::Style::Borderless);
+		_crosshairWidget->GetContentView()->AddSubview(_crosshair);
+		
+		_crosshairWidget->Open();
+		
+		UpdateCrosshair();
 	}
 	
 	void Application::WillExit()
 	{}
+	
+	
+	void Application::UpdateCrosshair()
+	{
+		RN::UI::Server *server = RN::UI::Server::GetSharedInstance();
+		
+		_crosshairWidget->SetFrame(RN::Rect(0.0f, 0.0f, server->GetWidth(), server->GetHeight()));
+		_crosshair->SetFrame(RN::Rect(0.0f, 0.0f, server->GetWidth(), server->GetHeight()));
+	}
 }
